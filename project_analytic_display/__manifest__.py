47c987d507dd4fb1793bb231930336128f75a541
Justin Mangini <justin@diyautospec.com>
2020-12-14 15:35:52 +0000

    transfer project_analytic_display

---
+++ project_analytic_display/__manifest__.py
@@ -0,0 +1,17 @@
+
+{
+    "name" : "Project Display Analytic Accounting",
+    "version" : "0.1",
+    "author" : "Justin Mangini",
+    'category': 'Project',
+    "depends" : ['project', 'hr_timesheet'],
+    "init_xml" : [],
+    "demo_xml" : [],
+    "description": "Project Display Analytic Accounting",
+    'data': ['views/views.xml'],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+    'certificate': '',
+    'images': [''],
+}
