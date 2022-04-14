d7af42155073ed81cd205fcc2aa07cc41918ecd7
Justin Mangini <justin@diyautospec.com>
2022-04-14 14:37:33 -0600

    add 520FORD 8,10 EA5050FORD1, 2

--- edi_highjump/models/import_hj.py
+++ edi_highjump/models/import_hj.py
@@ -566,7 +566,9 @@ class edi_highjump_import(models.Model):
                 
                 mo_id.action_confirm()
                 
-                if item[1] in ['520-FORD1', '520-FORD3','520-FORD4','520-FORD5','520-FORD6','520-FORD7','520-FORD9','520-FORD11', '530-FORD1', '530-FORD4','530-FORD5','530-FORD7', '530-FORD9', '530-FORD11']:
+                if item[1] in ['520-FORD1', '520-FORD3', '520-FORD4', '520-FORD5', '520-FORD6', '520-FORD7' , '520-FORD8', '520-FORD9', '520-FORD10', '520-FORD11', 
+                               '530-FORD1', '530-FORD4', '530-FORD5', '530-FORD7', '530-FORD9', '530-FORD11',
+                               'EA5050-FORD1', 'EA5050-FORD2']:
                     mo_id.button_plan()
                     
             except Exception as e:
